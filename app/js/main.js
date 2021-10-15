8265511c654a172be75b5ea29eb8d03abf27e799
ksulugol <lugol.k8@gmail.com>
2021-10-15 10:08:17 +0300

    added stock, brands, news, line-clamp

--- app/js/main.js
+++ app/js/main.js
@@ -5,17 +5,28 @@ $(function(){
     // autoplay: true
   });
 
-   $('.catalog').on('click', function(){
-    $('.catalog,.catalog__list').toggleClass('active');
+   $('.catalog__btn').on('click', function(){
+    $('.catalog, .catalog__list, .catalog__btn').toggleClass('active');
+    $('.catalog__list').slideToggle();
   });
+
+  $(document).on('click', function (e) {
+    var div = $('.catalog');
+    if (!div.is(e.target)
+        && div.has(e.target).length === 0) {
+      $('.catalog__list').slideUp();
+    }
+  });
+
 
   $('.catalog__link').on('click', function(){
-    $('.catalog__list').removeClass('active')
+    $('.catalog__list').slideUp();
   });
-
 
 
   var mixer = mixitup('.products__content');
+
+  var mixer = mixitup('.stock__content');
   
 
 });
