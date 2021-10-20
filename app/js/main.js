3fbfa6a4c5968338b521a04f065ad7ce27c7494d
ksulugol <lugol.k8@gmail.com>
2021-10-20 22:20:32 +0300

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -2,16 +2,36 @@ $(function(){
 
   $('.slider__items').slick({
     arrows: true,
-    // autoplay: true
+    dots: false,
+    // autoplay: true,
+     responsive: [{
+            breakpoint: 992,
+              settings: {
+                arrows: false,
+                dots: true
+              }
+    }]
   });
 
 
   $('.brands__list').slick({
-    slidesToShow: 5,
+    arrows: false,
+    slidesToShow: 6,
     slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
+    // autoplay: true,
+    autoplaySpeed: 2000,
+      responsive: [{
+          breakpoint: 992,
+            settings: {
+              slidesToShow: 4
+            },
+          breakpoint: 576,
+            settings: {
+              slidesToShow: 2
+            }
+      }]
   });
+
 
 
    $('.catalog__btn').on('click', function(){
@@ -32,11 +52,43 @@ $(function(){
     $('.catalog__list').slideUp();
   });
 
-
-  var mixer = mixitup('.products__content');
-
-  var mixer = mixitup('.stock__content');
   
+
+  $('.info__burger').on('click', function(){
+    $('.info__burger,.info__menu').toggleClass('active');
+    $('body').toggleClass('hidden');
+  });
+
+  $('.info__link').on('click', function(){
+    $('.info__burger,.info__menu').removeClass('active')
+    $('body').removeClass('hidden');
+  });
+
+
+
+   $('.header__search').on('click', function(){
+    $('.header__search,.form-mobile').toggleClass('active');
+    $('body').toggleClass('hidden');
+  });
+
+  $('.header__search').on('click', function(){
+    $('.header__search,.form-mobile').removeClass('active')
+    $('body').removeClass('hidden');
+  });
+
+
+
+  var containerEl1 = document.querySelector('[data-ref="container-1"]');
+  var containerEl2 = document.querySelector('[data-ref="container-2"]');
+
+  var config = {
+    controls: {
+      scope: 'local'
+    }
+  };
+
+  var mixer1 = mixitup(containerEl1, config);
+  var mixer2 = mixitup(containerEl2, config);
 
 });
 
